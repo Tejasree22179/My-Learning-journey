a2b7a2f0f748411ccb1fbc780784a6849bc3ab25
Tejasree22179 <gondralatejasree@gmail.com>
2025-07-27 23:47:15 +0530

    Add files via upload

---
+++ day1scenariobasedquestions.py
@@ -0,0 +1,12 @@
+'''name=input()
+age=int(input())
+sub1=float(input())
+sub2=float(input())
+sub3=float(input())
+print("the total marks is",sub1+sub2+sub3)
+print("the average is",(sub1+sub2+sub3)/3)'''
+#building a form
+name=input("enter the name:")
+age=int(input("enter the age:"))
+city=input("enter the city:")
+print(f" welcom eto python course{name} \n the data type of name is{type(name)}/n,the datatype of an age is{type(age)},and the city is{type(city)}")
